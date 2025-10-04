f1cfeca3f9904396a4515f61074b6824f7f4cd13
nelsonwm45 <nelsonwm45@gmail.com>
2025-10-05 01:56:01 +0800

    Added array.hpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -67,3 +67,36 @@ int	main()
 		std::cout << std::endl << std::endl << std::endl;
 	}
 }
+
+// From Evaluation Sheet
+// class Awesome
+// {
+//   public:
+//     Awesome( void ) : _n( 42 ) { return; }
+//     int get( void ) const { return this->_n; }
+//   private:
+//     int _n;
+// };
+
+// std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+// {
+//   o << rhs.get();
+//   return o;
+// }
+
+// template< typename T >
+// void print( T& x )
+// {
+//   std::cout << x << std::endl;
+//   return;
+// }
+
+// int main() {
+//   int tab[] = { 0, 1, 2, 3, 4 };
+//   Awesome tab2[5];
+
+//   iter( tab, 5, print<const int> );
+//   iter( tab2, 5, print<Awesome> );
+
+//   return 0;
+// }
